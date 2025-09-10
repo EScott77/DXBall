9a5adffdd2ad5f17519dd603742888b2f8de00f0
EScott77 <eescott@rollins.edu>
2025-09-10 11:51:59 -0400

    Add Wall Physics
    Created vector direction movements, more bricks

--- DXBall/Assets/Scripts/ball.cs
+++ DXBall/Assets/Scripts/ball.cs
@@ -16,13 +16,18 @@ void Update()
 rb.linearVelocity= direction*speed;
 }
 void OnTriggerEnter2D(Collider2D collison){
-// if (collison.gameObject.CompareTag("brick")){
-// direction.y = -direction.y;
-// Destroy(collison.gameObject);
-// }
-if (collison.gameObject.CompareTag("paddle")){
+if (collison.gameObject.CompareTag("brick")){
+direction.y = -direction.y;
+Destroy(collison.gameObject);
+} else if (collison.gameObject.CompareTag("paddle")){
     direction.y = -direction.y;
-}
+}else if (collison.gameObject.CompareTag("Wall")){
+    direction.x = -direction.x;
+}else if (collison.gameObject.CompareTag("Ceiling")){
+    direction.y = -direction.y;
+} else if (collison.gameObject.CompareTag("Floor")){
+    Debug.Log("Game Over");
+} 
 }
 }
 
